bf4f9799df9be50734bb8b8883154bfcc7d18c06
muhanadsoffo <muhanadsoffo2002@gmail.com>
2025-02-07 19:54:59 +0300

    creating the model and training it

--- train.py
+++ train.py
@@ -5,6 +5,8 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 from nltk_utils import tokenize,stem,bag_of_words
+
+from model import NeuralNet
 with open('intents.json','r') as f:
     intents =json.load(f)
 
@@ -45,14 +47,48 @@ class ChatDataset(Dataset):
 
 
     def __getitem__(self, index): #Returns one training example (features + label).
-        return self.x_data[idx] , self.y_data[idx]
+        return self.x_data[index] , self.y_data[index]
     
     def __len__(self): #Returns total number of training examples.
         return self.n_samples
     
 
 #hyperparameters
-batch_size=8
+batch_size= 8
+hidden_size= 8
+output_size= len(tags)
+input_size= len(x_train[0]) #we could also say length of the all_words
+learning_rate=0.001
+num_epochs=1000
+
 
 dataset=ChatDataset()
-train_loader=DataLoader (dataset=dataset, batch_size=batch_size, shuffle=True ,num_workers=2)
+train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True)
+
+device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model= NeuralNet(input_size,hidden_size, output_size).to(device)
+
+#loss and optimizer
+criterion= nn.CrossEntropyLoss()
+optimizer= torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+for epoch in range(num_epochs):
+    for (words, labels) in train_loader:
+        words=words.to(device)
+        labels = labels.to(device).long()
+
+        #forward
+
+        outputs=model(words)
+        loss =criterion(outputs,labels)
+
+        #backwards and optimizer step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    if (epoch+1) %100==0:
+        print(f'epoch {epoch+1}/{num_epochs},loss={loss.item():.4f}')
+
+
+print(f'final loss ,loss={loss.item():.4f}')
